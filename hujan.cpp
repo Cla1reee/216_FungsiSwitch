2cfd91de141dceaf70bb99526a2dabce8fe79abf
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-03-26 14:58:05 +0700

    first commit

---
+++ hujan.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int bilangan1, bilangan2;
+
+void inputData(){
+    cout << "Masukan bilangan pertama :";
+    cin >> bilangan1;
+    cout << "Masukana bilangan kedua ;";
+    cin >> bilangan2;
+}
+
+int penjumlahan(int a, int b){
+    return a +b;
+}
+
+switch (pilihan){
+    case 1:
+        inputData();
+        cout << "Hasil Penjumlahan = " << penjumlahan(bilangan1, bilangan2) << endl;
+        break
+
+}
+
+
+int main(){
+    int pilihan;
+    
+    do{
+        cout << "kalkulator  sederhana"<< endl; 
+        cout << "====================="<< endl;
+        cout << "1. Penjumlahan"<<endl;
+        cout << "2. Pengurangan"<<endl;
+        cout << "3. Perkalian"<<endl;
+        cout << "4. Pembagian"<<endl;
+        cout << "5. Keluar"<<endl;
+        cout << "====================="<< endl;
+        cout << "Masukan menu pilihan :";
+        cin>>pilihan;
+        
+        switch (pilihan) {
+    }
+
+}
+
+}
