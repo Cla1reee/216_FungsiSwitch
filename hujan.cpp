a6d4b321fb89224b2f4bd2d60162161462fd2af9
Hapis <152777554+Cla1reee@users.noreply.github.com>
2024-03-26 17:35:40 +0700

    intejer perhitungan

--- hujan.cpp
+++ hujan.cpp
@@ -10,6 +10,18 @@ void inputData(){
     cin >> bilangan2;
 }
 
+int penjumlahan(int a, int b){
+    return a +b;
+}
+int pengurangan (int a, int b) {
+    return a - b;
+}
+int perkalian (int a, int b) {
+    return a * b;
+}
+float  pembagian (int a , int b ) {
+}
+
+
 int main(){
-    
 }
